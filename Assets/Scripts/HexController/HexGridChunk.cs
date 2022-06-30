1e1a732291eed3f618d7d8bf7ee4024e7a137d6c
HenrickyL <Henrickyl1@gmail.com>
2022-06-30 04:21:53 -0300

    hexmap - Shore Water

--- Assets/Scripts/HexController/HexGridChunk.cs
+++ Assets/Scripts/HexController/HexGridChunk.cs
@@ -84,20 +84,28 @@ public class HexGridChunk: MonoBehaviour {
 		}
 	}
 
-    private void TriangulateWater (
+    void TriangulateWater (
 		HexDirection direction, HexCell cell, Vector3 center
 	) {
 		center.y = cell.WaterSurfaceY;
+		HexCell neighbor = cell.GetNeighbor(direction);
+		if (neighbor != null && !neighbor.IsUnderwater) {
+			TriangulateWaterShore(direction, cell, neighbor, center);
+		}
+		else {
+			TriangulateOpenWater(direction, cell, neighbor, center);
+		}
+	}
+
+    void TriangulateOpenWater (
+		HexDirection direction, HexCell cell, HexCell neighbor, Vector3 center
+	) {
 		Vector3 c1 = center + HexMetrics.GetFirstSolidCorner(direction);
 		Vector3 c2 = center + HexMetrics.GetSecondSolidCorner(direction);
 
 		water.AddTriangle(center, c1, c2);
-		if (direction <= HexDirection.SE) {
-			HexCell neighbor = cell.GetNeighbor(direction);
-			if (neighbor == null || !neighbor.IsUnderwater) {
-				return;
-			}
 
+		if (direction <= HexDirection.SE && neighbor != null) {
 			Vector3 bridge = HexMetrics.GetBridge(direction);
 			Vector3 e1 = c1 + bridge;
 			Vector3 e2 = c2 + bridge;
@@ -112,6 +120,35 @@ public class HexGridChunk: MonoBehaviour {
 					c2, e2, c2 + HexMetrics.GetBridge(direction.Next())
 				);
 			}
+		}
+		
+	}
+
+    void TriangulateWaterShore (
+		HexDirection direction, HexCell cell, HexCell neighbor, Vector3 center
+	) {
+		EdgeVertices e1 = new EdgeVertices(
+			center + HexMetrics.GetFirstSolidCorner(direction),
+			center + HexMetrics.GetSecondSolidCorner(direction)
+		);
+		water.AddTriangle(center, e1.v1, e1.v2);
+		water.AddTriangle(center, e1.v2, e1.v3);
+		water.AddTriangle(center, e1.v3, e1.v4);
+		water.AddTriangle(center, e1.v4, e1.v5);
+		Vector3 bridge = HexMetrics.GetBridge(direction);
+		EdgeVertices e2 = new EdgeVertices(
+			e1.v1 + bridge,
+			e1.v5 + bridge
+		);
+		water.AddQuad(e1.v1, e1.v2, e2.v1, e2.v2);
+		water.AddQuad(e1.v2, e1.v3, e2.v2, e2.v3);
+		water.AddQuad(e1.v3, e1.v4, e2.v3, e2.v4);
+		water.AddQuad(e1.v4, e1.v5, e2.v4, e2.v5);
+		HexCell nextNeighbor = cell.GetNeighbor(direction.Next());
+		if (nextNeighbor != null) {
+			water.AddTriangle(
+				e1.v5, e2.v5, e1.v5 + HexMetrics.GetBridge(direction.Next())
+			);
 		}
 	}
 
