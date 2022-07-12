6eefc966b6e332911f9d8c629a3361c2f882a8fd
HenrickyL <Henrickyl1@gmail.com>
2022-07-11 21:40:15 -0300

    value HP bar

--- Assets/Scripts/UI/StatusBar.cs
+++ Assets/Scripts/UI/StatusBar.cs
@@ -6,10 +6,12 @@ public class StatusBar : MonoBehaviour
 {
     public Slider HPSlider;
     public Slider ULTSlider;
+    public Text hpText;
 
     public void SetHealth(int health){
         if(health>0){
             HPSlider.value = health;
+            UpdateHP();
         }
     }
     public void SetUlt(int value){
@@ -20,9 +22,14 @@ public class StatusBar : MonoBehaviour
     public void SetMaxHealth(int value){
         HPSlider.maxValue = value;
         HPSlider.value = value;
+        UpdateHP();
     }
     public void SetMaxUlt(int value){
         ULTSlider.maxValue = value;
         ULTSlider.value = value;
     }
+
+    private void UpdateHP(){
+        hpText.text =  string.Format("{0}/{1}",HPSlider.value,HPSlider.maxValue);
+    } 
 }
